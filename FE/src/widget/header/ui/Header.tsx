ac2ae673420e9bdcd82760a7c097368fd4df0777
yuhyeon99 <jujini31@gmail.com>
2026-05-13 17:36:29 +0900

    style: 헤더 어두운 테마 스타일 적용

--- FE/src/widget/header/ui/Header.tsx
+++ FE/src/widget/header/ui/Header.tsx
@@ -1,23 +1,23 @@
-import logo from '@/assets/images/logo.png';
+import logo from '@/assets/images/logo.svg';
 
 export const Header = () => {
     return (
-        <header className="w-full border border-red-500 h-11 flex pl-3">
+        <header className="w-full h-11 flex pl-3 bg-[#333333] text-[#cccccc] text-sm select-none">
             <div className="logo flex items-center">
-              <img className="w-6.5 h-6.5" src={logo} alt="Logo" />
+              <img className="w-6.5" src={logo} alt="Logo" />
             </div>
             <div className="dropdown-menu flex relative h-full items-center pl-4">
               <ul className="flex h-full">
-                <li className="h-full relative border flex items-center group cursor-pointer px-2">
+                <li className="h-full relative flex items-center group px-2">
                   <p>Projects</p>
-                  <ul className="absolute top-full left-0 w-2xs border hidden group-hover:block">
+                  <ul className="absolute top-full left-0 w-2xs hidden group-hover:block cursor-pointer">
                     <li>New Projects</li>
                     <li>Open Projects</li>
                   </ul>
                 </li>
-                <li className="h-full relative border flex items-center group cursor-pointer px-2">
+                <li className="h-full relative flex items-center group px-2">
                   <p>Teams</p>
-                  <ul className="absolute top-full left-0 w-2xs border hidden group-hover:block">
+                  <ul className="absolute top-full left-0 w-2xs hidden group-hover:block cursor-pointer">
                     <li>Invite Member</li>
                     <li>Team Settings</li>
                     <li>Permissions</li>

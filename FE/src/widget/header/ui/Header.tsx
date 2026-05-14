1ce39a95f4fd537e9b7390519b8fe42f8b17c114
yuhyeon99 <jujini31@gmail.com>
2026-05-14 14:58:05 +0900

    feat: 헤더 컴포넌트 퍼블리싱

--- FE/src/widget/header/ui/Header.tsx
+++ FE/src/widget/header/ui/Header.tsx
@@ -2,7 +2,7 @@ import logo from '@/assets/images/logo.svg';
 
 export const Header = () => {
   return (
-    <header className="flex h-11 w-full bg-[#333333] pl-3 text-sm text-[#cccccc] select-none">
+    <header className="relative flex h-11 w-full bg-[#333333] pl-3 text-sm text-[#cccccc] select-none">
       <div className="logo flex items-center">
         <img className="w-6.5" src={logo} alt="Logo" />
       </div>
@@ -34,6 +34,14 @@ export const Header = () => {
             </ul>
           </li>
         </ul>
+      </div>
+      <div className="project-name absolute left-1/2 flex h-full -translate-x-1/2 items-center px-4">
+        <p>Project Name</p>
+      </div>
+      <div className="user-profile absolute right-3 flex h-full cursor-pointer items-center">
+        <div className="h-6.5 w-6.5 rounded-full bg-[#cccccc]">
+          {/* TODO: 사용자 아이콘을 여기에 추가 */}
+        </div>
       </div>
     </header>
   );
